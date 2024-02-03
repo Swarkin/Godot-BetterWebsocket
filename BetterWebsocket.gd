f9b82e3cef46fff273694b87d50a31c4eba1d613
fox <102416174+Swarkin@users.noreply.github.com>
2024-02-03 22:11:23 +0100

    Update BetterWebsocket.gd to v1.0.2

--- BetterWebsocket.gd
+++ BetterWebsocket.gd
@@ -1,7 +1,7 @@
 @icon('res://classes/BetterWebsocket.svg')
-extends Node
 class_name BetterWebsocket
-## Better Websocket Node v1.0.1 by swark1n
+extends Node
+## Better Websocket Node v1.0.2 by swark1n
 
 signal connected
 signal disconnected
@@ -11,33 +11,9 @@ signal packet_received(content: PackedByteArray)
 
 @export var verbose := false
 
-var s := WebSocketPeer.new()				## The underlying Websocket.
+var s := WebSocketPeer.new()		## The underlying Websocket.
 var _processing := false
-var _prev_state := s.STATE_CLOSED		## To get the Websocket state, use [code]better_websocket.s.get_ready_state()[/code] instead.
-
-
-func begin_connection(url: String) -> Error:
-	var err := s.connect_to_url(url)
-
-	if not err:
-		_processing = true
-		return err  # OK
-
-	var close_reason := s.get_close_reason()
-	var close_code := s.get_close_code()
-	connection_failed.emit(close_code, close_reason)
-	if verbose:
-		push_error('BetterWebsocket | Connection failed: ', error_string(err), ', code: ', close_code, ', reason: ', close_reason)
-
-	return err
-
-
-func close_connection(code := 1000) -> void:
-	s.close(code)
-
-
-func send_packet(content: String) -> Error:
-	return s.put_packet(content.to_utf8_buffer())
+var _prev_state := s.STATE_CLOSED	## To get the Websocket state, use [code]better_websocket.s.get_ready_state()[/code] instead.
 
 
 func _process(_dt: float) -> void:
@@ -62,3 +38,25 @@ func _process(_dt: float) -> void:
 		s.STATE_CLOSED:
 			disconnected.emit()
 			_processing = false
+
+
+func begin_connection(url: String) -> Error:
+	var err := s.connect_to_url(url)
+
+	if not err:
+		_processing = true
+		return err  # OK
+
+	var close_reason := s.get_close_reason()
+	var close_code := s.get_close_code()
+	connection_failed.emit(close_code, close_reason)
+	if verbose:
+		push_error('BetterWebsocket | Connection failed: ', error_string(err), ', code: ', close_code, ', reason: ', close_reason)
+
+	return err
+
+func close_connection(code := 1000) -> void:
+	s.close(code)
+
+func send_packet(content: String) -> Error:
+	return s.put_packet(content.to_utf8_buffer())
